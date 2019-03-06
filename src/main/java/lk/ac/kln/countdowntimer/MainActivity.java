447f8dbaa83685499740299a563ff767e25b4e45
milinda45 <milinda45@gmail.com>
2019-03-06 15:28:05 +0530

    added a button

--- src/main/java/lk/ac/kln/countdowntimer/MainActivity.java
+++ src/main/java/lk/ac/kln/countdowntimer/MainActivity.java
@@ -1,13 +1,28 @@
 package lk.ac.kln.countdowntimer;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int Counter =99;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CountDown();
     }
+
+    private void CountDown () {
+        final TextView textview= findViewById(R.id.textView);
+        final Handler Handler = new Handler();
+        Handler.post(new Runnable(){
+            @Override
+            public void run() {
+                textview.setText(Integer.toString(Counter));
+                Counter--;
+                Handler.postDelayed(this, 1000);
+        }
+    });
 }
